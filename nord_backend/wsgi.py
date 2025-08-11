30168bb78684f8c0ca39b3d4d7424a0051be8879
dcaffaro <129459384+dcaffaro@users.noreply.github.com>
2025-08-11 15:14:08 -0300

    v4

--- nord_backend/wsgi.py
+++ nord_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nord-backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nord_backend.settings')
 
 application = get_wsgi_application()
