8124b0d1f68dbc3b1998d46ba8dc96cca86dece3
Douglas Caffaro <dcaffaro@vincipartners.com>
2025-09-01 18:20:25 -0300

    Config ML Models

--- nord_backend/urls.py
+++ nord_backend/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     re_path(r'^(?P<tenant_id>[^/]+)/', include('hr.urls')),
     re_path(r'^(?P<tenant_id>[^/]+)/', include('multitenancy.urls')),
     re_path(r'^(?P<tenant_id>[^/]+)/', include('billing.urls')),
-    re_path(r'^(?P<tenant_id>[^/]+)/', include('ml.urls')),
+    re_path(r'^(?P<tenant_id>[^/]+)/', include('ML.urls')),
     #path('api/', include('accounting.urls')),
     path(r'^api/token/?$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path(r'^api/token/refresh/?$', TokenRefreshView.as_view(), name='token_refresh'),
